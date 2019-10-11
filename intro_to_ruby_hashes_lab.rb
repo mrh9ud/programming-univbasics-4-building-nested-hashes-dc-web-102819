db17f069ac1a754deff76fa02f2b0e8619a35fe7
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-11 15:36:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,8 +8,7 @@ end
 
 def monopoly_with_second_tier
 	hash = {
-	  :railroads => {:pieces => 4, :rent_in_dollars => 
-	    {
+	  :railroads => {:pieces => 4
 	  }
 	}
 end
@@ -17,10 +16,15 @@ end
 def monopoly_with_third_tier
 	hash = {
 	  :railroads => {
-	    :pieces => 4
-	    
+	    :pieces => 4, :rent_in_dollars => 
+	      {
+	      :one_piece_owned => "25",
+	      :two_pieces_owned => "50",
+	      :three_pieces_owned => "100",
+	      :four_pieces_owned => "200"
+	      }
+	    }
 	  }
-	}
 end
 
 def monopoly_with_fourth_tier
